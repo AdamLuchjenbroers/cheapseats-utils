ea9a8875e6be80ab5a3052491d039eeffa30355e
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2021-04-21 21:19:25 +1000

    Include deploy in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,4 @@ zip -j Package/Lambda-NetworkInfo.zip NetworkInfo/*
 KMS_S3KEY=`aws kms describe-key --key-id alias/aws/s3 --query 'KeyMetadata.KeyId' | tr -d \"`
 
 aws cloudformation package --template-file utils.cf.json --s3-bucket deimos-configmgmt --s3-prefix Build/Artifacts/Urila --kms-key-id $KMS_S3KEY --output-template-file Package/utils.cf.json
+aws cloudformation deploy --template-file Package/utils.cf.json --stack-name deimos-utils --capabilities CAPABILITY_NAMED_IAM
