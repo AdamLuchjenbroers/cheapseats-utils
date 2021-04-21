45cfe2d1afe650bbd13d1d45603bb270a403b650
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2021-04-21 21:19:56 +1000

    Return untransformed snippet for initial testing run

--- NetworkInfo/NetworkInfo.py
+++ NetworkInfo/NetworkInfo.py
@@ -2,4 +2,9 @@
 def macro_handler(event, context):
     print(event)
     print(context)
-    
+ 
+    return {
+        "requestId" : event["requestId"]
+    ,   "status" : "success"
+    ,   "fragment": event['fragment']
+    }   
