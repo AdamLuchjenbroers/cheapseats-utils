f1ccfbecdc9b1610825992df3aba7a7d2eaaae92
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2021-04-23 20:07:29 +1000

    Remove hard-coded stack name

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Simple build and deployment script, since this is too
 # basic to warrant its own pipeline yet.
@@ -9,5 +9,14 @@ KMS_S3KEY=`aws kms describe-key --key-id alias/aws/s3 --query 'KeyMetadata.KeyId
 S3_BUCKET=`aws ssm get-parameter --name "/CheapSeats/Env/ConfigBucket" --query "Parameter.Value" | tr -d \"`
 S3_PREFIX=`aws ssm get-parameter --name "/CheapSeats/Env/ConfigBucket/ArtifactPrefix" --query "Parameter.Value" | tr -d \"`
 
+source .config
+
+if [ -z ${STACKNAME+X} ]; then
+   echo 'Name of Stack to Deploy?:'
+   read STACKNAME
+
+   echo STACKNAME=${STACKNAME} > .config
+fi
+
 aws cloudformation package --template-file utils.cf.json --s3-bucket ${S3_BUCKET} --s3-prefix ${S3_PREFIX}/Utils --kms-key-id $KMS_S3KEY --output-template-file Package/utils.cf.json
-aws cloudformation deploy --template-file Package/utils.cf.json --stack-name deimos-utils --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file Package/utils.cf.json --stack-name ${STACKNAME} --capabilities CAPABILITY_NAMED_IAM
